c5dad37caa01c52727186db1f4b267083c566565
jzhupan <jzhupan@gmail.com>
2023-08-02 17:15:10 -0400

    Fixed some css issues on the navigation bar.

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -7,9 +7,9 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar expand="lg" className="bg-body-tertiary nav-bar">
+      <Container fluid className="nav-bar">
+        <Navbar.Brand href="#">FMP</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -18,25 +18,36 @@ const NavBar = () => {
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
+            <NavDropdown title="Education" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action2">
+                Stock market indexes
               </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
+              <NavDropdown.Item href="#action3">
+                Financial Ratios
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="#action4">
+                Cryptocurrency
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <NavDropdown title="Markets" id="navbarScrollingDropdown-two">
+              <NavDropdown.Item href="#action5">
+                Most Searched Stocks
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action6">
+                Market Indexes: Major Markets
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="#action7">
+                Stock Market Top Gainers
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#">Theme</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
               type="search"
-              placeholder="Search"
+              placeholder="Search stock..."
               className="me-2"
               aria-label="Search"
             />
