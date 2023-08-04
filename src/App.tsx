da074cbe46bee4625992ae778e26034e1bceb071
jzhupan <jzhupan@gmail.com>
2023-08-04 18:59:42 -0400

    Tried to fetch photo data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Layout from "./Components/Layout";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+//import styled from 'styled-components';
+
 
 interface User {
   id: number;
@@ -10,15 +12,30 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('')
 
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+    const fetchUsers = async () => {
+      try {
+        const res = await axios
+          .get<User[]>("https://jsonplaceholder.typicode.com/photos")
+        setUsers(res.data)
+      }
+      catch (err) {
+        setError((err as AxiosError).message)
+      }
+    } 
+
+    fetchUsers()
+    //get -> await promise -> res / err
+
+      // .then((res) => setUsers(res.data))
+      // .catch(err => setError(err.message))
   }, []);
 
   return (
     <>
+    {error && <p className="text-danger">{error}</p>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
