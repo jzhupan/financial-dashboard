4da102cfb18070d556ecd3c443f0ac99d7e9a1b5
jzhupan <jzhupan@gmail.com>
2023-08-03 21:11:45 -0400

    Trying to understand useEffect and refreshing memory of fetching data with axios.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Layout from "./Components/Layout";
+import axios from "axios";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
   return (
     <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
       <Layout />
     </>
   );
