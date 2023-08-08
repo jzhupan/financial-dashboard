8b9c5124fde3a6eb8904cdf99508e677c4f33b15
jzhupan <jzhupan@gmail.com>
2023-08-08 17:50:26 -0400

    Added placeholders for SideBar

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -5,7 +5,38 @@ import Container from "react-bootstrap/Container";
 const SideBar = () => {
   return (
     <Container className="sidebar-container" fluid>
-
+      <Row>
+        <Col > 
+        <span>1 A short title.</span>
+        </Col>
+        <Col >
+        <img src="https://via.placeholder.com/50/3D1D73/0000FF" />
+        </Col>
+        <Col > 
+        <span>2 A short title.</span>
+        </Col>
+        <Col >
+        <img src="https://via.placeholder.com/50/3D1D73/0000FF" />
+        </Col>
+        <Col > 
+        <span>3 A short title.</span>
+        </Col>
+        <Col >
+        <img src="https://via.placeholder.com/50/3D1D73/0000FF" />
+        </Col>
+        <Col > 
+        <span>4 A short title.</span>
+        </Col>
+        <Col >
+        <img src="https://via.placeholder.com/50/3D1D73/0000FF" />
+        </Col>
+        <Col > 
+        <span>5 A short title.</span>
+        </Col>
+        <Col >
+        <img src="https://via.placeholder.com/50/3D1D73/0000FF" />
+        </Col>
+      </Row>
     </Container>
   );
 };
