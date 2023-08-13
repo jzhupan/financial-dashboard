7a5d068802392451fc90be22ccd3748ddb8e0baf
jzhupan <jzhupan@gmail.com>
2023-08-12 22:58:14 -0400

    Attempted to fetch data from News api.

--- src/Components/NewsSection.tsx
+++ src/Components/NewsSection.tsx
@@ -1,13 +1,66 @@
-import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from "react-bootstrap/Container";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
+// interface User {
+//   id: number,
+//   name: string,
+// }
+// JSON.stringify(import.meta.env.VITE_REACT_APP_USERS_URL)
+
+
+// const SideBar = () => {
+//      const [users, setUsers] = useState<User[]>([])
+    
+//      useEffect(() => {
+//       axios.get<User[]>((import.meta.env.VITE_REACT_APP_USERS_URL))
+//         .then((res) => {
+          
+//          setUsers(res.data)})
+//         .catch((error) => {
+//           if(error.response) {
+//             console.log(error.response.data)
+//           }
+//         })
+//      }, [])
+
+//   return (
+//     // <Container className="sidebar-container" fluid>
+      
+//     // </Container>
+//     <ul>
+//       {users.map(user => <li key={user.id}>{user.name}</li>)}
+//     </ul>
+//   );
+// };
+
+interface News {
+    title: string,
+    description: string,
+    snippet: string,
+    url: string,
+    imageUrl: string,
+    published_at: string,
+    source: string,
+    uuid: number
+}
 
 const NewsSection = () => {
+    const [topNews, setTopNews] = useState<News[]>([])
+
+    useEffect(() => {
+        axios.get<News[]>((import.meta.env.VITE_REACT_APP_NEWS_SAMPLES))
+        .then((res) => {
+            setTopNews(res.data)
+        })
+    })
+
   return (
     <Container fluid className='news-container'>
-        <Row className='news-row'>
+
+        {/* <Row className='news-row'>
             <h1>News</h1>
         </Row>
         <Row className='news-row'>
@@ -41,7 +94,7 @@ const NewsSection = () => {
                 <p>This is where the paragraph news</p>
                 <span>Date</span>
             </Col>
-        </Row>
+        </Row> */}
     </Container>
   )
 }
--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -5,38 +5,6 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 
-// interface User {
-//   id: number,
-//   name: string,
-// }
-// JSON.stringify(import.meta.env.VITE_REACT_APP_USERS_URL)
-
-
-// const SideBar = () => {
-//      const [users, setUsers] = useState<User[]>([])
-    
-//      useEffect(() => {
-//       axios.get<User[]>((import.meta.env.VITE_REACT_APP_USERS_URL))
-//         .then((res) => {
-          
-//          setUsers(res.data)})
-//         .catch((error) => {
-//           if(error.response) {
-//             console.log(error.response.data)
-//           }
-//         })
-//      }, [])
-
-//   return (
-//     // <Container className="sidebar-container" fluid>
-      
-//     // </Container>
-//     <ul>
-//       {users.map(user => <li key={user.id}>{user.name}</li>)}
-//     </ul>
-//   );
-// };
-
 //const myUrl = (import.meta.env.VITE_REACT_APP_BASE_URL)
 
  interface Gainer {
@@ -60,9 +28,9 @@ const SideBar = () => {
     <Container className="sidebar-container" fluid>
       {mostGainers.map(mostGainer => 
       <Row>
-        <Col><h6>{mostGainer.symbol}</h6></Col>
-        <Col><h6>{mostGainer.changesPercentage.toFixed(2)}%</h6></Col>
-        <Col><h6>${mostGainer.price}</h6></Col>
+        <Col key={mostGainer.id}><h6>{mostGainer.symbol}</h6></Col>
+        <Col key={mostGainer.id}><h6>{mostGainer.changesPercentage.toFixed(2)}%</h6></Col>
+        <Col key={mostGainer.id}><h6>${mostGainer.price}</h6></Col>
       </Row>)}
     </Container>
 
