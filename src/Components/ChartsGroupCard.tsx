9b7b9ff0f88dc10fc51f8135c2eeed32003281b5
jzhupan <jzhupan@gmail.com>
2023-08-22 12:19:17 -0400

    managed to place a functional areachart

--- src/Components/ChartsGroupCard.tsx
+++ src/Components/ChartsGroupCard.tsx
@@ -3,7 +3,6 @@ import Container from "react-bootstrap/Container";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Col from "react-bootstrap/esm/Col";
-
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 
@@ -33,7 +32,7 @@ const ChartsGroupCard = () => {
   
  
   return (
-    <Container className="carousel-container">
+    <Container className="carousel-container">      
       {error && <p className="text-danger">{error}</p>}
       {priceChangeList.map((priceChange) =>(
 
@@ -42,27 +41,35 @@ const ChartsGroupCard = () => {
             <Card.Body>
             <Col className="chart">
                 {/* <Card.Img variant="right" src={"https://placehold.jp/100x50.png"} /> */}
-                {priceChangeList && priceChangeList.length < 0 && (
-                  <ResponsiveContainer>
-                  <AreaChart
-                    width={100}
-                    height={50}
-                    data={priceChangeList}
-                  >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey={priceChange.timestamp} />
-                    <YAxis />
-                    <Tooltip />
-                    <Area type="monotone" dataKey={priceChange.price} stroke="#8884d8" fill="#8884d8" />
-                  </AreaChart>
+
+                {priceChangeList && priceChangeList.length > 0 && new Date(priceChange.timestamp) && (
+              //     <ResponsiveContainer width={200} height={100}>
+              //     <AreaChart
+              //       width={100}
+              //       height={50}
+              //       data={priceChangeList}
+              //     >
+              //       <CartesianGrid strokeDasharray="3 3" />
+              //       <XAxis dataKey="timestamp" />
+              //       <YAxis />
+              //       <Tooltip />
+              //       <Area type="monotone" dataKey="price" stroke="#8884d8" fill="#8884d8" />
+              //     </AreaChart>
+              // </ResponsiveContainer>
+                <ResponsiveContainer width="100%" height={100}>
+                <AreaChart
+                  width={100}
+                  height={50}
+                  data={priceChangeList}
+                >
+                  <Area type="monotone" dataKey="price" stroke="#8884d8" fill="#8884d8" />
+                </AreaChart>
               </ResponsiveContainer>
-                )}
+                )}   
               </Col>
               <Col>
                 <Card.Title>{priceChange.symbol.replace("^", "")}</Card.Title>
-
               </Col>
-              
               <Col>
               <small>
                 +{priceChange.price}$ 
