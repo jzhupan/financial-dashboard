5a554c8be4c2672d234ed6f5de0e9687ff4013ca
jzhupan <jzhupan@gmail.com>
2023-10-08 22:25:02 -0400

    Restored app to working state

--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,11 @@
 import ResponsiveAppBar from "./Global/AppBar"
 import RealTimeStockPrices from "./Global/Marquee"
 import PageLayout from "./Global/PageLayout"
-import handleSubmit from './Global/handles/handleSubmit';
-import { useRef } from 'react';
+
 
 
 
 export default function Home() {
-
-  const dataRef = useRef()
- 
-  const submitHandler = (e) => {
-    e.preventDefault()
-    handleSubmit(dataRef.current.value)
-    dataRef.current.value = ""
-  }
-
 
   return (
   <>
